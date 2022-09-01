2d102f6117601f375f25d919a1e30b354efd55c0
Luis Garcia <luis_grdz@hotmail.com>
2022-08-31 23:49:24 -0500

    Creación de grid dinámico a 40 por 40 de manera recursiva y sin utilizar if ni bucles e intentando small methods

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Game.css';
 
+function Game () {
+    const [grid, setGrid] = useState([])
 
-const CELL_SIZE = 20;
-const WIDTH = 800;
-const HEIGHT = 600;
+    const COLS = 40;
+    const ROWS = 40;
 
+    // MÉTODO RECURSIVO
+    const crearGrid = (contador, grid) => {
+        const rowInterna  = [];
+        //base case
+        switch (contador) {
+            case ROWS:
+                return grid;
+        
+            default:
+                grid.push(crearRow(0, rowInterna));
+                return crearGrid(contador + 1, grid);
+        }
+    }
+
+    // MÉTODO RECURSIVO 
+    const crearRow = (contador, row) => {
+        switch (contador) {
+            case COLS:
+                return row;
+        
+            default:
+                row.push(Math.floor(Math.random() * 2))
+                return crearRow(contador + 1, row);
+        }
+    }
+
+    useEffect(() => {
+        let griCreado = crearGrid(0, grid)
+        setGrid(griCreado)
+
+        console.log(grid)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default Game;
